39347ce7f5ac12d5d99d36a3ad5886d9824df49e
sumiya-banu <sumiyadudekula2002@gmail.com>
2025-09-29 11:44:52 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,7 +5,11 @@ int main()
     printf("Welcome to noveltronix");
     printf("hello gowri");
 <<<<<<< HEAD
+<<<<<<< HEAD
     printf("hello");
 =======
 >>>>>>> d16dc38 (Update hello.c)
+=======
+    printf("hello");
+>>>>>>> b776713 (Update hello.c)
 }
